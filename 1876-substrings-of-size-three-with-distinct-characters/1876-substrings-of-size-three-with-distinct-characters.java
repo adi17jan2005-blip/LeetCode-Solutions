295366476506d2fb64ecdbecb71e418f9a7aba88
Aditya Mohan <adi17jan2005@gmail.com>
2026-05-07 22:34:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,5 +1,3 @@
-import java.util.HashSet;
-
 class Solution {
 
     public int countGoodSubstrings(String s) {
@@ -8,27 +6,14 @@ class Solution {
 
         for (int i = 0; i <= s.length() - 3; i++) {
 
-            String sub = s.substring(i, i + 3);
+            if (s.charAt(i) != s.charAt(i + 1) &&
+                s.charAt(i) != s.charAt(i + 2) &&
+                s.charAt(i + 1) != s.charAt(i + 2)) {
 
-            if (dupe(sub)) {
                 count++;
             }
         }
 
         return count;
-    }
-
-    public boolean dupe(String a) {
-
-        HashSet<Character> h = new HashSet<>();
-
-        for (int i = 0; i < a.length(); i++) {
-
-            if (!h.add(a.charAt(i))) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
