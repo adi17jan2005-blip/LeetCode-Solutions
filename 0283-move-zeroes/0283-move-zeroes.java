6034e7375ab10006db769c4d356753e8561cab6d
Aditya Mohan <adi17jan2005@gmail.com>
2026-08-14 19:13:16 +0530

    Time: 2 ms (91.97%), Space: 47.7 MB (76.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != 0) {
+        int j=-1;
+        for(int k=0;k<nums.length;k++){
+            if(nums[k]==0){
+                 j=k;
+                break;
+            }
+        }
+        if(j==-1){
+            return;
+        }
+//i keeps the track of the non zero no thata need to be swaped by the jth index which tracs the 0's
+        for (int i = j+1; i < nums.length;i++) {
+            if (nums[i] != 0) {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                i++;
+                j++;
             }
         }
     }
